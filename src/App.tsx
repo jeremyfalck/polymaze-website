11e5a787f27f239c79111df481fd18b44a7e8ef5
jeremyfalck <jeremy.falck@proton.me>
2023-10-16 13:37:49 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./header/Navbar";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBar />,
+  },
+  {
+    path: "/projects",
+    element: (
+      <>
+        <NavBar />
+        <div>projects</div>
+      </>
+    ),
+  },
+  {
+    path: "/team",
+    element: (
+      <>
+        <NavBar />
+        <div>team</div>
+      </>
+    ),
+  },
+  {
+    path: "/contact",
+    element: (
+      <>
+        <NavBar />
+        <div>contact</div>
+      </>
+    ),
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
