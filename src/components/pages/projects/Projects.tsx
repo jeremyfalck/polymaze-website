cc524b90afe010c09c19cdcf32ca620dfdde0661
jeremyfalck <j.falck-ext@ubitransport.com>
2023-10-29 20:13:45 +0100

    reworked responsiveness

--- src/components/pages/projects/Projects.tsx
+++ src/components/pages/projects/Projects.tsx
@@ -35,57 +35,64 @@ export default function Projects() {
           height={dimensions.height - headerHeight}
         />
         <div
-          className="mx-auto py-6 sm:px-6 lg:px-8 w-full flex-1 flex-col justify-center absolute top-0 left-0 overscroll-contain overflow-y-auto"
+          className="w-full mx-auto py-6 sm:px-6 lg:px-8 flex-1 flex-col justify-center absolute top-0 left-0 overscroll-contain overflow-y-auto"
           style={{
             maxHeight: dimensions.height,
             paddingTop: headerHeight,
           }}
         >
-          <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
-            Nouveau single: Take your hand
-          </p>
-          <iframe
-            className="w-full sm:w-3/5 lg:w-2/5 rounded-lg mx-auto sm:mx-0 sm:text-5xl px-6 sm:px-0 "
-            height={152}
-            src="https://open.spotify.com/embed/album/3aLOe8ePJqIUMqUgNziMcV?utm_source=generator"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-            allowFullScreen
-          />
-          <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
-            Live au Nadir
-          </p>
-          <iframe
-            className="w-full sm:w-3/5 lg:w-2/5 rounded-lg mx-auto sm:mx-0 px-6 sm:px-0"
-            style={{ aspectRatio: "16/9" }}
-            src="https://www.youtube.com/embed/nqe7BnNznPY?si=XJ7Pudh0rGLM7GAg"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          />
-          <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
-            Notre EP : Another Step
-          </p>
-          <iframe
-            className="w-full sm:w-3/5 lg:w-2/5 rounded-lg mx-auto sm:text-5xl sm:mx-0 px-6 sm:px-0"
-            src="https://open.spotify.com/embed/album/5auF6rrW1uOUA1VJpTESz1?utm_source=generator"
-            height="352"
-            frameBorder="0"
-            allowFullScreen
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            loading="lazy"
-          />
-          <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
-            Live dans notre salon: Fondation
-          </p>
-          <iframe
-            className="w-full sm:w-3/5 lg:w-2/5 rounded-lg mx-auto sm:mx-0 px-6 sm:px-0"
-            style={{ aspectRatio: "16/9" }}
-            src="https://www.youtube.com/embed/777ITCO7BSA?si=kyZamaVvm18iFmrq"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          />
+          <div className="flex flex-col lg:flex-row">
+            <div className="flex-1">
+              <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
+                Nouveau single: Take your hand
+              </p>
+              <iframe
+                className="w-full lg:w-3/4 rounded-lg mx-auto sm:mx-0 sm:text-5xl px-6 sm:px-0 "
+                height={152}
+                src="https://open.spotify.com/embed/album/3aLOe8ePJqIUMqUgNziMcV?utm_source=generator"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+                allowFullScreen
+              />
+
+              <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
+                Notre EP : Another Step
+              </p>
+              <iframe
+                className="w-full lg:w-3/4 rounded-lg mx-auto sm:text-5xl sm:mx-0 px-6 sm:px-0"
+                src="https://open.spotify.com/embed/album/5auF6rrW1uOUA1VJpTESz1?utm_source=generator"
+                height="352"
+                frameBorder="0"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy"
+              />
+            </div>
+            <div className="flex-1 sm:px-6">
+              <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
+                Live au Nadir
+              </p>
+              <iframe
+                className="w-full lg:w-3/4 rounded-lg mx-auto sm:mx-0 px-6 sm:px-0"
+                style={{ aspectRatio: "16/9" }}
+                src="https://www.youtube.com/embed/nqe7BnNznPY?si=XJ7Pudh0rGLM7GAg"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              />
+              <p className="text-white order-first text-3xl font-semibold tracking-tight p-6 sm:p-0 sm:py-6">
+                Live dans notre salon: Fondation
+              </p>
+              <iframe
+                className="w-full lg:w-3/4 rounded-lg mx-auto sm:mx-0 px-6 sm:px-0"
+                style={{ aspectRatio: "16/9" }}
+                src="https://www.youtube.com/embed/777ITCO7BSA?si=kyZamaVvm18iFmrq"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              />
+            </div>
+          </div>
         </div>
         <NavBar index={1} ref={ref} />
       </main>
